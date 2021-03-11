f560e1e66852352ec23bbe194ffaa4b5c40c128b
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2021-03-11 10:45:54 +0900

    Challenge Day11-12: Unit Test - Mocking

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { async } from '../../node_modules/rxjs';
+import { User } from './entites/user.entity';
+import { Repository } from '../../node_modules/typeorm';
+import { getRepositoryToken } from '../../node_modules/@nestjs/typeorm';
+import { JwtService } from '../jwt/jwt.service';
 
-describe('UsersService', () => {
+type MockRepository<T=any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
+describe('UserService', () => {
   let service: UsersService;
+  let usersRepository: MockRepository<User>;
+
+  const MockRepository = {
+    findOne: jest.fn(),
+    save: jest.fn(),
+    create: jest.fn(),
+  };
+
+  const mockJwtRepository = {
+    sign: jest.fn(),
+    verify: jest.fn(),
+  };
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+    const module = await Test.createTestingModule({
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: MockRepository
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtRepository
+        }
+      ],
     }).compile();
-
     service = module.get<UsersService>(UsersService);
+    usersRepository = module.get(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
-});
+
+})
