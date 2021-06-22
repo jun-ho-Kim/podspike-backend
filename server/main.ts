f14ed36fae43b146821c676a163820ec0def7b7c
jun-ho-Kim <wnsgh5049@naver.com>
2021-06-22 15:54:06 +0900

    enableCols()

--- server/main.ts
+++ server/main.ts
@@ -3,8 +3,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  console.log("process.env.PORT", process.env.PORT)
+  const app = await NestFactory.create(AppModule);;
+  app.enableCors();
   await app.listen(process.env.PORT || 5000);
 }
 
